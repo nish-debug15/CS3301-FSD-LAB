a5279ce52eabb33f1334b803ae69d0c94354030c
nish-debug15 <patelnishit118@email.com>
2026-08-19 10:28:42 +0530

    lab1 setup

---
+++ experiment-1/script.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Experiment 1!");
+console.warn("This is a sample warning message.");
+console.error("This is a sample error message for demonstration.");
+
+document.getElementById('click-me-btn').addEventListener('click', function() {
+    console.log("Button clicked!");
+    alert("You clicked the button!");
+});
